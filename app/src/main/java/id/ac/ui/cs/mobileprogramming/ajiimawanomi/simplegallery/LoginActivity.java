a91e8d8bbce560f0334707413157eb3ee483e186
cymon1997 <cybermonster82@yahoo.com>
2018-12-14 19:27:29 +0700

    added login page

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/ajiimawanomi/simplegallery/LoginActivity.java
@@ -0,0 +1,14 @@
+package id.ac.ui.cs.mobileprogramming.ajiimawanomi.simplegallery;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
