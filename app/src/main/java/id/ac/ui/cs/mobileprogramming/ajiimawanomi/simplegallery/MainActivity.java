313d039b2ae8bb74e09835aae0f01b7046d0d253
cymon1997 <cybermonster82@yahoo.com>
2018-12-21 00:01:56 +0700

    added working fragment

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/ajiimawanomi/simplegallery/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/ajiimawanomi/simplegallery/MainActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -34,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.layout_drawer);
         setDrawer();
 
+        setFragment(new GalleryFragment());
+
         authViewModel = ViewModelProviders.of(this).get(AuthViewModel.class);
 
         Observer<BaseResponse<FirebaseUser>> authObserver = new Observer<BaseResponse<FirebaseUser>>() {
@@ -44,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         };
 
         authViewModel.getInstance().observe(this, authObserver);
-        
+
         checkData();
     }
 
@@ -109,5 +114,13 @@ public class MainActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
